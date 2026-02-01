d0f702b2e6f580ce02c1988004f091b888f96319
quirky-sharan <imsharansoni@gmail.com>
2026-02-01 14:51:34 +0530

    Updated structure

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfid } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [
+      react(),
+    tailwindcss(),
+  ],
+})
